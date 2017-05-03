46b6f2a2b4853342460a7cff017b7270b1bc1202
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-05-03 15:59:24 +0000

    Add project files.

---
+++ CloudAtlas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CloudAtlas.MvcApplication" Language="C#" %>
---
+++ CloudAtlas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CloudAtlas.Startup))]
+namespace CloudAtlas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

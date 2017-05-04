e3811bd13c6b5b3420ae924ddf23a0612964dd5b
Kolbeinn Ari Arnórsson <kolbeinna16@ru.is>
2017-05-04 11:24:44 +0000

    Dashboardid redirectar a rett drasl

--- CloudAtlas/Controllers/DashboardController.cs
+++ CloudAtlas/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CloudAtlas.Models;
 
 namespace CloudAtlas.Controllers
 {
@@ -11,7 +12,24 @@ namespace CloudAtlas.Controllers
         // GET: Dashboard
         public ActionResult Index()
         {
-            return View();
+            List<Project> proj = new List<Project>
+            {
+                new Project{ID = 0, Name = "Veff"},
+                new Project{ID = 1, Name = "Gagn"},
+                new Project{ID = 2, Name = "WhatUp"},
+                new Project{ID = 0, Name = "Veff"}
+            };
+            List<Group> grou = new List<Group>
+            {
+                new Group{ID = 0, Name = "Gagnaskipan"},
+                new Group{ID = 1, Name = "Vefforritun"},
+                new Group{ID = 2, Name = "Ramadan"},
+                new Group{ID = 3, Name = "Allah"}
+            };
+
+            DashboardViewModel model = new DashboardViewModel{ Projects = proj, Groups = grou};
+
+            return View(model);
         }
 
         public ActionResult Settings()
---
+++ CloudAtlas/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudAtlas.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Folder> Folders { get; set; }
+    }
+}

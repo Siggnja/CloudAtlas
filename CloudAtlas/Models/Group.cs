51cd71527355a6dfbe92fe9e34c401134e627af2
Jóhann Helgi Gunnarsson <johannhg16@ru.is>
2017-05-04 17:16:40 +0000

    database and model classes added

---
+++ CloudAtlas/Models/Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudAtlas.Models
+{
+    public class Group
+    {
+        public int ID { get; set; }
+
+        public int OwnerID { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual IEnumerable<User> Users { get; set; }
+
+        public virtual IEnumerable<Project> Projects { get; set; }
+
+
+
+    }
+}

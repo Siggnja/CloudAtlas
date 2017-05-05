3b9e9cc29c2273303ad6d54cba144896ba728157
Sverrir <sverrirs15@ru.is>
2017-05-05 16:13:46 +0000

    Ná í email úr database
    Nær reyndar bara í fyrsta email, en fínt reference til að græja restina

--- CloudAtlas/Controllers/ProjectController.cs
+++ CloudAtlas/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CloudAtlas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,15 @@ namespace CloudAtlas.Controllers
 {
     public class ProjectController : Controller
     {
+        ApplicationDbContext context = new ApplicationDbContext();
         // GET: Project
         public ActionResult Index()
         {
-            return View();
+            var model = (from item in context.Users
+                        select item).FirstOrDefault();
+
+
+            return View(model);
         }
     }
 }

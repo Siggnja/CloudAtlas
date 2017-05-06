a10a99958b1efd599e35b21f05a98bce43956acf
Bulli <siggnja@gmail.com>
2017-05-06 15:56:06 +0000

    Only authenticated users can access the dashboard now

--- CloudAtlas/Controllers/AccountController.cs
+++ CloudAtlas/Controllers/AccountController.cs
@@ -57,8 +57,7 @@ namespace CloudAtlas.Controllers
         [AllowAnonymous]
         public ActionResult Login(string returnUrl)
         {
-            ViewBag.ReturnUrl = returnUrl;
-            return View();
+            return RedirectToAction("Index", "Home");
         }
 
         //
@@ -68,27 +67,7 @@ namespace CloudAtlas.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-            switch (result)
-            {
-                case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
-                case SignInStatus.LockedOut:
-                    return View("Lockout");
-                case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-                case SignInStatus.Failure:
-                default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
-                    return View(model);
-            }
+            return RedirectToAction("Index", "Home");
         }
 
         //
@@ -327,7 +306,7 @@ namespace CloudAtlas.Controllers
            
             if (loginInfo == null)
             {
-                return RedirectToAction("Login");
+                return RedirectToAction("Index","Home");
             }
        
             // Sign in the user with this external login provider if the user already has a login
--- CloudAtlas/Controllers/DashboardController.cs
+++ CloudAtlas/Controllers/DashboardController.cs
@@ -7,29 +7,15 @@ using CloudAtlas.Models;
 using System.Web.Security;
 using Microsoft.AspNet.Identity;
 
+
 namespace CloudAtlas.Controllers
 {
+    [Authorize]
     public class DashboardController : Controller
     {
         // GET: Dashboard
         public ActionResult Index()
         {
-            /*
-            List<Project> proj = new List<Project>
-            {
-                new Project{ID = 0, Name = "Hello World"},
-                new Project{ID = 1, Name = "Project X"},
-                new Project{ID = 2, Name = "Facebook 2.0"},
-                new Project{ID = 4, Name = "Verklegt 2"}
-            };
-            List<Group> grou = new List<Group>
-            {
-                new Group{ID = 0, Name = "Gagnaskipan"},
-                new Group{ID = 1, Name = "Vefforritun"},
-                new Group{ID = 2, Name = "Ramadan"},
-                new Group{ID = 3, Name = "Allah"}
-            };
-            */
 
             string id = User.Identity.GetUserId<string>();
             ApplicationDbContext context = new ApplicationDbContext();

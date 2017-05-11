09de2fea3b117d9d8def753155815401ac52dba1
kolliarnorsson <kolbeinna16@ru.is>
2017-05-11 16:39:24 +0000

    Validate a settings

--- CloudAtlas/Models/Folder.cs
+++ CloudAtlas/Models/Folder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,8 @@ namespace CloudAtlas.Models
     public class Folder
     {
         public int ID { get; set; }
-
+        [Required]
+        //[StringLength(30,ErrorMessage = "Folder name can not be longer then 30 characters")]
         public string Name { get; set; }
 
         public bool IsRoot { get; set; }

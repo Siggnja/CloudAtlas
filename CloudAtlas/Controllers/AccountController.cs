ba719f8cf3362297640964597481e23137c96e58
Jóhann Helgi Gunnarsson <johannhg16@ru.is>
2017-05-11 23:12:05 +0000

    No longer possible to upload files larger than 4mb, and  image fix

--- CloudAtlas/Controllers/AccountController.cs
+++ CloudAtlas/Controllers/AccountController.cs
@@ -348,7 +348,7 @@ namespace CloudAtlas.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, Theme = "dawn", AvatarPath= "https://res.cloudinary.com/cloudatlas/image/upload/v1494448981/Default.png" };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, Theme = "dawn", AvatarPath= "http://res.cloudinary.com/cloudatlas/image/upload/v1494533241/Default.png" };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {

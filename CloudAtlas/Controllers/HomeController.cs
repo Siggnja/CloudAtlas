575ad364207e7154f6f77b51848a8cdba6d842e9
Sverrir <sverrirs15@ru.is>
2017-05-12 01:31:45 +0000

    Cleanup
    It never ends!

--- CloudAtlas/Controllers/HomeController.cs
+++ CloudAtlas/Controllers/HomeController.cs
@@ -19,22 +19,9 @@ namespace CloudAtlas.Controllers
             {
                 return RedirectToAction("Index", "Dashboard");
             }
-
-            var i = (from item in context.Users
-                     select item);
               return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-        public ActionResult Chat()
-        {
-            return View();
-        }
         public ActionResult Login(string provider,string returnUrl)
         {
             return RedirectToAction("ExternalLogin","Account", new { provider = provider, returnurl = returnUrl });
